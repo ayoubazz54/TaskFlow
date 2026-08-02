63610a05eda3ffd09d7f3364a9baa700c878e50e
ayoubass54 <ayoubelaz542@gmail.com>
2026-08-02 23:04:04 +0200

    Creation de base de donnée, et connection avec postgresql.

---
+++ tryNodejs/database/db.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+    host: "localhost",
+    port: "5432",
+    user: "postgres",
+    password: "1234",
+    database: "taskflow"
+});
+
+module.exports = pool;
