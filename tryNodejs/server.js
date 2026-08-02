71521e32c3c4502af2f2d257809070177c91d514
ayoubass54 <ayoubelaz542@gmail.com>
2026-08-02 17:01:26 +0200

    Suppression des comentaires.

--- tryNodejs/server.js
+++ tryNodejs/server.js
@@ -12,31 +12,3 @@ app.use(tasksRoutes);
 app.listen(3000, () => {
     console.log("Serveur lancé.");
 });
-
-
-
-
-
-
-
-/*
-app.get("/about", (req, res) => {
-    res.send("A propos")
-});
-
-app.get("/contact", (req, res) => {
-    res.send("Contact")
-});
-
-
-
-app.get('/users', (req, res) => {
-    res.json([{
-        "id": 1,
-        "name": "Ayoub"
-    },
-    {
-        "id": 2,
-        "name": "Alice"
-    }]);
-}) */
